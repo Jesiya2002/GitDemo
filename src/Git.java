d4ab6db0744d9e63c3acb6a3045d9108d7c869b6
Jesiya2002 <JESIYA-ARPUTHAM.D@CAPGEMINI.COM>
2024-12-18 10:05:53 +0530

    Update Git.java

--- src/Git.java
+++ src/Git.java
@@ -3,6 +3,7 @@ public class Git {
 	public static void main(String[] args) {
 		System.out.println("Hello");
         System.out.println("STS message for branch");
+	System.out.println("GitHub message for branch");
 	}
 
 }
